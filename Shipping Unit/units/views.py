31f8ceb3c69edef6968c1d45da14f148260ae485
LPennicott <LPennicott3@gmail.com>
2020-05-21 17:42:52 -0400

    Settings/Views
    Formmated Views.py file to be more in line with PEP standards.
    Copied settings file from deployed site.

--- Shipping Unit/units/views.py
+++ Shipping Unit/units/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView, FormView
 from django.core.mail import send_mail
-from django.conf import settings
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
@@ -13,9 +12,11 @@ import datetime, csv
 from .models import Shipping_Units, InboundUnits
 from .forms import InboundForm
 
+
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
 
 class UnitListView(LoginRequiredMixin, ListView):
     model = Shipping_Units
@@ -23,7 +24,7 @@ class UnitListView(LoginRequiredMixin, ListView):
     template_name = 'units/unit_list.html'
     ordering = ['-on_hand']
     login_url = 'account_login'
-    
+
 
 class UnitDetailView(LoginRequiredMixin, DetailView):
     model = Shipping_Units
@@ -31,26 +32,36 @@ class UnitDetailView(LoginRequiredMixin, DetailView):
     template_name = 'units/unit_detail.html'
     login_url = 'account_login'
 
+
 class UnitUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Shipping_Units
-    fields = ('client', 'sub_client', 'width', 'length', 'height', 'gross_weight', 'purpose',)
+    fields = (
+        'client', 'sub_client', 'width', 'length',
+        'height', 'gross_weight', 'purpose',
+    )
     template_name = 'units/unit_edit.html'
     login_url = 'account_login'
     permission_required = ('units.can_modify')
 
-class UnitDeleteView(LoginRequiredMixin, PermissionRequiredMixin,DeleteView):
+
+class UnitDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Shipping_Units
     template_name = 'units/unit_delete.html'
     success_url = reverse_lazy('unit_list')
     login_url = 'account_login'
     permission_required = ('units.can_modify')
 
+
 class UnitCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Shipping_Units
     template_name = 'units/unit_new.html'
-    fields = ('client', 'sub_client', 'width', 'length', 'height', 'gross_weight', 'purpose',)
+    fields = (
+        'client', 'sub_client', 'width', 'length',
+        'height', 'gross_weight', 'purpose',
+    )
     login_url = 'account_login'
     permission_required = ('units.can_add_shipment')
+
 
 class UnitReleaseView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Shipping_Units
@@ -63,13 +74,15 @@ class UnitReleaseView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
         unit.save()
         return redirect('unit_detail', unit.pk)
 
+
 class UnitReleasePage(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Shipping_Units
     context_object_name = 'unit_list'
     template_name = 'units/units_release.html'
-    queryset = Shipping_Units.objects.filter(release_date = None)
+    queryset = Shipping_Units.objects.filter(release_date=None)
     login_url = 'account_login'
     permission_required = ('units.can_modify')
+
 
 class UnitsReleaseView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Shipping_Units
@@ -78,39 +91,44 @@ class UnitsReleaseView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
 
     def post(self, request, *args, **kwargs):
         units = request.POST.getlist('onhands[]')
-        released_units = Shipping_Units.objects.filter(on_hand__in = units)\
-            .update(release_date = datetime.datetime.today())
+        Shipping_Units.objects.filter(on_hand__in=units)\
+            .update(release_date=datetime.datetime.today())
         return redirect('unit_list')
+
 
 class ExportListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Shipping_Units
     context_object_name = 'unit'
-    queryset = Shipping_Units.objects.filter(release_date = datetime.datetime.today())
+    queryset = Shipping_Units.objects.filter(
+        release_date=datetime.datetime.today())
     login_url = 'account_login'
     permission_required = ('units.can_modify')
 
     def render_to_response(self, context, **response_kwargs):
         unit = context.get('unit')
-        units = unit.values_list('client', 
-            'width', 
-            'length', 
-            'height', 
-            'gross_weight', 
+        units = unit.values_list(
+            'client',
+            'width',
+            'length',
+            'height',
+            'gross_weight',
             'purpose',
-            )
-        response = HttpResponse(content_type = 'text/csv')
+        )
+        response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename = "units.csv"'
         writer = csv.writer(response)
-        writer.writerow(['Client', 
-            'Width', 
-            'Length', 
-            'Height', 
-            'Gross Weight', 
+        writer.writerow([
+            'Client',
+            'Width',
+            'Length',
+            'Height',
+            'Gross Weight',
             'Purpose'
-            ])
+        ])
         for unit in units:
             writer.writerow(unit)
         return response
+
 
 class SearchResultsListView(LoginRequiredMixin, ListView):
     model = Shipping_Units
@@ -122,37 +140,43 @@ class SearchResultsListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         query = self.request.GET.get('q')
         return Shipping_Units.objects.filter(
-            Q(client__icontains = query) | Q(on_hand__contains = query) | Q(purpose__icontains = query) |\
-            Q(mawb__icontains = query) | Q(hawb__icontains = query)
+            Q(client__icontains=query) | Q(on_hand__contains=query) | Q(purpose__icontains=query) |\
+            Q(mawb__icontains=query) | Q(hawb__icontains=query)
         )
+
 
 class CreateConsolView(LoginRequiredMixin, ListView):
     model = Shipping_Units
     context_object_name = 'unit_list'
     template_name = 'units/consolidation.html'
-    queryset = Shipping_Units.objects.exclude(release_date = None)\
-    .filter(mawb = None, purpose = 'Export')
+    queryset = Shipping_Units.objects.exclude(release_date=None)\
+    .filter(mawb=None, purpose='Export')
     login_url = 'account_login'
+
 
 class Consolidation(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Shipping_Units
     login_url = 'account_login'
     permission_required = ('units.can_edit_delivery')
-    
+
     def post(self, request, *args, **kwargs):
         units = request.POST.getlist('onhands[]')
         mawb = request.POST.get('mawb')
         hawb = request.POST.get('hawb')
-        Shipping_Units.objects.filter(on_hand__in = units).update(mawb = mawb)
-        Shipping_Units.objects.filter(on_hand__in = units).update(hawb = hawb)
+        Shipping_Units.objects.filter(on_hand__in=units).update(mawb=mawb)
+        Shipping_Units.objects.filter(on_hand__in=units).update(hawb=hawb)
         return redirect('unit_list')
+
 
 @login_required
 def delivery_list(request):
     deliveries = InboundUnits.objects.order_by('-date_received', 'pk')
-    return render(request,
-                    'inbound/delivery_list.html',
-                    {'deliveries': deliveries})
+    return render(
+        request,
+        'inbound/delivery_list.html',
+        {'deliveries': deliveries}
+    )
+
 
 @permission_required('units.can_add_delivery')
 @login_required
@@ -166,32 +190,40 @@ def new_delivery(request):
         form = InboundForm()
     return render(request, 'inbound/new_delivery.html', {'form': form})
 
+
 @permission_required('units.can_edit_delivery')
 @login_required
 def edit_delivery(request, pk):
-    record = get_object_or_404(InboundUnits, pk = pk)
+    record = get_object_or_404(InboundUnits, pk=pk)
     if request.method == 'POST':
-        form = InboundForm(request.POST, instance = record)
+        form = InboundForm(request.POST, instance=record)
         if form.is_valid():
             form.save()
             return redirect('/inbound/delivery_list/')
     else:
-        form = InboundForm(instance = record)
-    return render(request, 'inbound/edit_delivery.html', {'form': form,
-                                                         'record': record})
+        form = InboundForm(instance=record)
+    return render(
+        request,
+        'inbound/edit_delivery.html',
+        {'form': form, 'record': record}
+    )
+
+
 @permission_required('units.can_edit_delivery')
 @login_required
 def email_delivery(request):
     total = InboundUnits.objects.filter(
-        date_received = datetime.datetime.today()).aggregate(Sum('unit_count')
+        date_received=datetime.datetime.today()).aggregate(Sum('unit_count')
     )
     sum_delivery = total['unit_count__sum']
     subject = f'Pallets Received {datetime.datetime.today().strftime("%Y-%m-%d")}'
     message = f'Good Afternoon,\n\nWe received {sum_delivery} pallets today.'
     email_from = 'ACPShipping_Unit@aircitypost.com'
-    recipient_list = ['lpennicott@aircitypost.com', 
-    'JRalph@aircitypost.com', 
-    'SMasterson@aircitypost.com',
-    'NCraigen@aircitypost.com',]
-    send_mail( subject, message, email_from, recipient_list )
+    recipient_list = [
+        'lpennicott@aircitypost.com',
+        'JRalph@aircitypost.com',
+        'SMasterson@aircitypost.com',
+        'NCraigen@aircitypost.com'
+    ]
+    send_mail(subject, message, email_from, recipient_list)
     return redirect('/inbound/delivery_list/')
