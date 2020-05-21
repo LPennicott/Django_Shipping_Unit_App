c67243fc7fbb0a750f82b46b1391ac09657fec00
Leighton <LPennicott3@gmail.com>
2020-05-21 18:00:00 -0400

    Formatting for PEP

--- Shipping Unit/units/admin.py
+++ Shipping Unit/units/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from .models import Shipping_Units, InboundUnits
 # Register your models here.
+
+
 class UnitAdmin(admin.ModelAdmin):
     list_display = ('on_hand', 'client', 'create_date',)
     ordering = ('on_hand',)
 
+
 class InboundUnitAdmin(admin.ModelAdmin):
     list_display = ('unit_count', 'date_received',)
     ordering = ('date_received',)
+
 
 admin.site.register(Shipping_Units, UnitAdmin)
 admin.site.register(InboundUnits, InboundUnitAdmin)
--- Shipping Unit/units/models.py
+++ Shipping Unit/units/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.urls import reverse
-from django.shortcuts import redirect
-import datetime
 # Create your models here.
+
 
 class Shipping_Units(models.Model):
     STATUS_CHOICES = (
@@ -11,44 +10,49 @@ class Shipping_Units(models.Model):
         ('Other', 'Other')
     )
     on_hand = models.AutoField(
-        primary_key = True,
-        editable = False,
+        primary_key=True,
+        editable=False,
     )
-    client = models.CharField(max_length = 50)
-    sub_client = models.CharField(max_length = 50, null = True, blank = True)
+    client = models.CharField(max_length=50)
+    sub_client = models.CharField(max_length=50, null=True, blank=True)
     width = models.PositiveIntegerField()
     length = models.PositiveIntegerField()
     height = models.PositiveIntegerField()
     gross_weight = models.PositiveIntegerField()
-    purpose = models.CharField(max_length = 10,
-                                choices = STATUS_CHOICES,
-                                default = 'Other')
-    create_date = models.DateField(auto_now_add = True)
-    release_date = models.DateField(null = True, blank = True)
-    hawb = models.CharField(max_length = 50, null = True, blank = True)
-    mawb = models.CharField(max_length = 50, null = True, blank = True)
+    purpose = models.CharField(max_length=10,
+                               choices=STATUS_CHOICES,
+                               default='Other')
+    create_date = models.DateField(auto_now_add=True)
+    release_date = models.DateField(null=True, blank=True)
+    hawb = models.CharField(max_length=50, null=True, blank=True)
+    mawb = models.CharField(max_length=50, null=True, blank=True)
 
     class Meta:
         ordering = ('release_date',)
-        permissions = (('can_add_shipment', 'Can add a shipping unit'),
-                        ('can_modify', 'Can modify a shipment'),
-                        ('can_release', 'Can release a shipment'),
-                        ('can_delete', 'Can erase a shipment from existence'))
+        permissions = (
+                       ('can_add_shipment', 'Can add a shipping unit'),
+                       ('can_modify', 'Can modify a shipment'),
+                       ('can_release', 'Can release a shipment'),
+                       ('can_delete', 'Can erase a shipment from existence')
+                    )
 
     def __str__(self):
         return self.client
 
     def get_absolute_url(self):
-        return reverse('unit_detail', args = [str(self.on_hand)])
+        return reverse('unit_detail', args=[str(self.on_hand)])
+
 
 class InboundUnits(models.Model):
     unit_count = models.PositiveIntegerField()
-    date_received = models.DateField(auto_now_add = True)
+    date_received = models.DateField(auto_now_add=True)
 
     class Meta:
         ordering = ('date_received',)
-        permissions = (('can_add_delivery', 'Can add delivery'),
-                        ('can_edit_delivery', 'Can edit delivery'))
+        permissions = (
+                       ('can_add_delivery', 'Can add delivery'),
+                       ('can_edit_delivery', 'Can edit delivery')
+                    )
 
     def __str__(self):
         return f'UPS {self.date_received} - {self.unit_count}'
